4a8b857cd70019eab3e3b683c657c074026fb798
elsiff <elsiff1002@gmail.com>
2020-03-01 21:09:47 +0900

    Fix issue response object of dishes have mongodb values

--- src/models/meal.js
+++ src/models/meal.js
@@ -2,15 +2,24 @@ import mongoose from "mongoose";
 
 const Schema = mongoose.Schema;
 
+const dishSchema = new Schema({
+    foodName: {type: String, required: true},
+    amount: {type: Number, default: 1}
+}, {
+    toJSON: {
+        transform: function (doc, ret) {
+            delete ret._id;
+            delete ret.__v;
+        }
+    }
+});
+
 const mealSchema = new Schema({
     username: {type: String, index: true, unique: true, required: true},
     date: {type: Date, index: true, required: true},
     location: String,
     satisfactionScore: {type: Number, min: 0, max: 4},
-    dishes: [{
-        foodName: {type: String, required: true},
-        amount: {type: Number, default: 1}
-    }]
+    dishes: [dishSchema]
 }, {
     toJSON: {
         transform: function (doc, ret) {
