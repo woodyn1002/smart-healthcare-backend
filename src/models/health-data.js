a2097fc2f46f2ab4d5e7a0366cb998469bd08358
elsiff <elsiff1002@gmail.com>
2020-03-19 21:05:29 +0900

    Add calculated properties on the response of health data api

--- src/models/health-data.js
+++ src/models/health-data.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import moment from "moment";
 
 const Schema = mongoose.Schema;
 
@@ -28,8 +29,71 @@ const healthDataSchema = new Schema({
         transform: function (doc, ret) {
             delete ret._id;
             delete ret.__v;
-        }
+        },
+        virtuals: true
     }
+});
+
+healthDataSchema.virtual('age').get(function () {
+    if (this.birthdate && this.birthdate.date) {
+        return moment().year() - moment(this.birthdate.date).year() + 1;
+    }
+    return undefined;
+});
+
+healthDataSchema.virtual('bmr').get(function () {
+    if (this.age && this.weight && this.height && this.sex) {
+        if (this.sex === 'male')
+            return 66.47 + (13.75 * this.weight) + (5 * this.height) - (6.76 * this.age);
+        else if (this.sex === 'female')
+            return 65.51 + (9.56 * this.weight) + (1.85 * this.height) - (4.68 * this.age);
+    }
+    return undefined;
+});
+
+healthDataSchema.virtual('bmi').get(function () {
+    if (this.weight && this.height) {
+        let heightInMeters = this.height / 100;
+        let bmi = this.weight / (heightInMeters * heightInMeters);
+        return Math.floor(bmi * 100) / 100;
+    }
+    return undefined;
+});
+
+healthDataSchema.virtual('bmiState').get(function () {
+    if (this.bmi === undefined) return undefined;
+    else if (this.bmi < 18.5) return 'underweight';
+    else if (this.bmi < 23) return 'normal';
+    else if (this.bmi < 25) return 'overweight';
+    else if (this.bmi < 30) return 'obese';
+    else return 'extremelyObese';
+});
+
+healthDataSchema.virtual('abnormalFields').get(function () {
+    let fields = [];
+
+    if (this.waist &&
+        ((this.sex === 'male' && this.waist >= 90) ||
+         (this.sex === 'female' && this.waist >= 85))) {
+        fields.push('waist');
+    }
+    if (this.bloodPressure &&
+        (this.bloodPressure.min >= 85 || this.bloodPressure.max >= 130)) {
+        fields.push('bloodPressure');
+    }
+    if (this.neutralFat && this.neutralFat >= 150) {
+        fields.push('neutralFat');
+    }
+    if (this.hdlCholesterol &&
+        ((this.sex === 'male' && this.hdlCholesterol < 40) ||
+         (this.sex === 'female' && this.hdlCholesterol < 50))) {
+        fields.push('hdlCholesterol');
+    }
+    if (this.fastingBloodSugar && this.fastingBloodSugar >= 100) {
+        fields.push('fastingBloodSugar');
+    }
+
+    return fields;
 });
 
 export default mongoose.model('HealthData', healthDataSchema, 'healthdata');
