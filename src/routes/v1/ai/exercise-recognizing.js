b082178809f33bc34320c258ac84d6451f0e5567
elsiff <elsiff1002@gmail.com>
2020-05-14 16:48:14 +0900

    Change exercise-recognizing apis to support additional exercises

--- src/routes/v1/ai/exercise-recognizing.js
+++ src/routes/v1/ai/exercise-recognizing.js
@@ -1,23 +1,31 @@
 import express from "express";
-const modelJson = require("../../../tf_models/exercise/model.json");
-const metadataJson = require("../../../tf_models/exercise/metadata.json");
-const weightsBin = require("../../../tf_models/exercise/weights.bin");
+
+const kneePushUpModelJson = require("../../../tf_models/exercise/knee-push-up/model.json");
+const kneePushUpMetadataJson = require("../../../tf_models/exercise/knee-push-up/metadata.json");
+const kneePushUpWeightsBin = require("../../../tf_models/exercise/knee-push-up/weights.bin");
+
+const squatModelJson = require("../../../tf_models/exercise/squat/model.json");
+const squatMetadataJson = require("../../../tf_models/exercise/squat/metadata.json");
+const squatWeightsBin = require("../../../tf_models/exercise/squat/weights.bin");
+
+const standingSideCrunchModelJson = require("../../../tf_models/exercise/standing-side-crunch/model.json");
+const standingSideCrunchMetadataJson = require("../../../tf_models/exercise/standing-side-crunch/metadata.json");
+const standingSideCrunchWeightsBin = require("../../../tf_models/exercise/standing-side-crunch/weights.bin");
 
 const router = express.Router();
 
-router.get('/model',
-    (req, res) => {
-        res.download(__dirname + modelJson.default);
-    });
+router.get('/knee-push-up/model', (req, res) => res.download(__dirname + kneePushUpModelJson.default));
+router.get('/knee-push-up/metadata', (req, res) => res.download(__dirname + kneePushUpMetadataJson.default));
+router.get('/knee-push-up/weights.bin', (req, res) => res.download(__dirname + kneePushUpWeightsBin.default));
 
-router.get('/metadata',
-    (req, res) => {
-        res.download(__dirname + metadataJson.default);
-    });
+router.get('/squat/model', (req, res) => res.download(__dirname + squatModelJson.default));
+router.get('/squat/metadata', (req, res) => res.download(__dirname + squatMetadataJson.default));
+router.get('/squat/weights.bin', (req, res) => res.download(__dirname + squatWeightsBin.default));
 
-router.get('/weights.bin',
-    (req, res) => {
-        res.download(__dirname + weightsBin.default);
-    });
+router.get('/standing-side-crunch/model', (req, res) => res.download(__dirname + standingSideCrunchModelJson.default));
+router.get('/standing-side-crunch/metadata', (req, res) => res.download(__dirname +
+                                                                        standingSideCrunchMetadataJson.default));
+router.get('/standing-side-crunch/weights.bin', (req, res) => res.download(__dirname +
+                                                                           standingSideCrunchWeightsBin.default));
 
 export default router;
