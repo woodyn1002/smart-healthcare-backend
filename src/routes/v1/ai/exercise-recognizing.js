b89185e87de57145aa2e8e6f09d90d7b3b37be6a
elsiff <elsiff1002@gmail.com>
2020-05-14 19:25:21 +0900

    Change webpack config to provide tf_models folder

--- src/routes/v1/ai/exercise-recognizing.js
+++ src/routes/v1/ai/exercise-recognizing.js
@@ -1,31 +1,26 @@
 import express from "express";
 
-const kneePushUpModelJson = require("../../../tf_models/exercise/knee-push-up/model.json");
-const kneePushUpMetadataJson = require("../../../tf_models/exercise/knee-push-up/metadata.json");
-const kneePushUpWeightsBin = require("../../../tf_models/exercise/knee-push-up/weights.bin");
-
-const squatModelJson = require("../../../tf_models/exercise/squat/model.json");
-const squatMetadataJson = require("../../../tf_models/exercise/squat/metadata.json");
-const squatWeightsBin = require("../../../tf_models/exercise/squat/weights.bin");
-
-const standingSideCrunchModelJson = require("../../../tf_models/exercise/standing-side-crunch/model.json");
-const standingSideCrunchMetadataJson = require("../../../tf_models/exercise/standing-side-crunch/metadata.json");
-const standingSideCrunchWeightsBin = require("../../../tf_models/exercise/standing-side-crunch/weights.bin");
-
 const router = express.Router();
 
-router.get('/knee-push-up/model', (req, res) => res.download(__dirname + kneePushUpModelJson.default));
-router.get('/knee-push-up/metadata', (req, res) => res.download(__dirname + kneePushUpMetadataJson.default));
-router.get('/knee-push-up/weights.bin', (req, res) => res.download(__dirname + kneePushUpWeightsBin.default));
+router.get('/knee-push-up/model',
+    (req, res) => res.download("./dist/tf_models/exercise/knee-push-up/model.json"));
+router.get('/knee-push-up/metadata',
+    (req, res) => res.download("./dist/tf_models/exercise/knee-push-up/metadata.json"));
+router.get('/knee-push-up/weights.bin',
+    (req, res) => res.download("./dist/tf_models/exercise/knee-push-up/weights.bin"));
 
-router.get('/squat/model', (req, res) => res.download(__dirname + squatModelJson.default));
-router.get('/squat/metadata', (req, res) => res.download(__dirname + squatMetadataJson.default));
-router.get('/squat/weights.bin', (req, res) => res.download(__dirname + squatWeightsBin.default));
+router.get('/squat/model',
+    (req, res) => res.download("./dist/tf_models/exercise/squat/model.json"));
+router.get('/squat/metadata',
+    (req, res) => res.download("./dist/tf_models/exercise/squat/metadata.json"));
+router.get('/squat/weights.bin',
+    (req, res) => res.download("./dist/tf_models/exercise/squat/weights.bin"));
 
-router.get('/standing-side-crunch/model', (req, res) => res.download(__dirname + standingSideCrunchModelJson.default));
-router.get('/standing-side-crunch/metadata', (req, res) => res.download(__dirname +
-                                                                        standingSideCrunchMetadataJson.default));
-router.get('/standing-side-crunch/weights.bin', (req, res) => res.download(__dirname +
-                                                                           standingSideCrunchWeightsBin.default));
+router.get('/standing-side-crunch/model',
+    (req, res) => res.download("./dist/tf_models/exercise/standing-side-crunch/model.json"));
+router.get('/standing-side-crunch/metadata',
+    (req, res) => res.download("./dist/tf_models/exercise/standing-side-crunch/metadata.json"));
+router.get('/standing-side-crunch/weights.bin',
+    (req, res) => res.download("./dist/tf_models/exercise/standing-side-crunch/weights.bin"));
 
 export default router;
--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require("path");
 const webpack = require("webpack");
 const nodeExternals = require('webpack-node-externals');
 const DotenvFlow = require('dotenv-flow-webpack');
+const CopyPlugin = require('copy-webpack-plugin');
 require("babel-polyfill");
 
 module.exports = {
@@ -25,18 +26,16 @@ module.exports = {
                 use: {
                     loader: 'babel-loader'
                 }
-            },
-            {
-                test: /\.(json|bin)$/,
-                loader: 'file-loader',
-                type: 'javascript/auto'
             }
         ]
     },
     plugins: [
         new webpack.IgnorePlugin(/\.(css|less)$/),
         new webpack.BannerPlugin({banner: 'require("source-map-support").install();', raw: true, entryOnly: false}),
-        new DotenvFlow()
+        new DotenvFlow(),
+        new CopyPlugin([
+            { from: 'src/tf_models', to: 'tf_models' }
+        ])
     ],
     devtool: 'source-map',
     mode: 'development'
